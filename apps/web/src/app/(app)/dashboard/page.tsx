bd9b04695e9a941011137e6fc7c47f3dde810400
Harsh Shrikant Bhat <90265455+harshsbhat@users.noreply.github.com>
2024-10-01 13:06:45 +0530

    Internal package @helplink/db

--- apps/web/src/app/(app)/dashboard/page.tsx
+++ apps/web/src/app/(app)/dashboard/page.tsx
@@ -1,12 +1,31 @@
 import Navbar from "@/components/Navbar";
-import Client from "./client"
+import Client from "./client";
+import { db, schema } from "@helplink/db"; 
 
-export default function Applications() {
+type Post = {
+  id: number;
+  name: string; 
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+const posts = schema.posts;
+
+async function getData(): Promise<Post[]> {
+  // Fetch the data from the database and assert the type
+  const allPosts = await db.select().from(posts) as Post[]; // Assert type here
+  return allPosts;
+}
+
+export default async function Applications() {
+  const postData = await getData();
+  console.log(postData);
+
   return (
-    <div >
-    <Navbar >
+    <div>
+      <Navbar>
       <Client />
-    </Navbar>
+      </Navbar>
     </div>
-  )
+  );
 }
